80871f11da636736e4d7c8ccbdb73b4386589713
maftukhatul arifin <maftukhatularifin13@gmail.com>
2019-06-03 14:20:00 +0700

    Add URL manipulation @Path

--- app/src/main/java/com/example/parsingjsonretrofit/JsonPlaceholderAPI.java
+++ app/src/main/java/com/example/parsingjsonretrofit/JsonPlaceholderAPI.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface JsonPlaceholderAPI {
 
     @GET("posts")
     Call<List<Posts>> getPosts();
+
+    @GET("posts/{id}/comments")
+    Call<List<Comments>> getComments(@Path("id") int postId);
 
 }
--- app/src/main/java/com/example/parsingjsonretrofit/MainActivity.java
+++ app/src/main/java/com/example/parsingjsonretrofit/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.parsingjsonretrofit;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.List;
@@ -15,6 +17,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvPostResult;
+    ProgressBar pbLoadingData;
+
+    JsonPlaceholderAPI jsonPlaceholderAPI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +27,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvPostResult = (TextView) findViewById(R.id.tv_post);
+        pbLoadingData = (ProgressBar) findViewById(R.id.pb_loading_data);
+
+        showLoading();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        JsonPlaceholderAPI jsonPlaceholderAPI = retrofit.create(JsonPlaceholderAPI.class);
+        jsonPlaceholderAPI = retrofit.create(JsonPlaceholderAPI.class);
 
+        getComments();
+
+    }
+
+    private void getPosts(){
         Call<List<Posts>> call = jsonPlaceholderAPI.getPosts();
         call.enqueue(new Callback<List<Posts>>() {
             @Override
@@ -50,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
 
                     tvPostResult.append(result);
+                    hideLoding();
                 }
 
             }
@@ -57,7 +71,53 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<List<Posts>> call, Throwable t) {
                 tvPostResult.setText(t.getMessage());
+                hideLoding();
             }
         });
     }
+
+    private void getComments(){
+        Call<List<Comments>> call = jsonPlaceholderAPI.getComments(1);
+        call.enqueue(new Callback<List<Comments>>() {
+            @Override
+            public void onResponse(Call<List<Comments>> call, Response<List<Comments>> response) {
+
+                if (!response.isSuccessful()) {
+                    tvPostResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Comments> comments = response.body();
+                for (Comments comment : comments) {
+                    String result = "";
+                    result += "postId: " + comment.getPostId() + "\n";
+                    result += "ID: " + comment.getId() + "\n";
+                    result += "name: " + comment.getName() + "\n";
+                    result += "email: " + comment.getEmail() + "\n";
+                    result += "body: " + comment.getText() + "\n\n";
+
+                    tvPostResult.append(result);
+                    hideLoding();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comments>> call, Throwable t) {
+                tvPostResult.setText(t.getMessage());
+                hideLoding();
+            }
+        });
+    }
+
+
+    private void showLoading() {
+        tvPostResult.setVisibility(View.INVISIBLE);
+        pbLoadingData.setVisibility(View.VISIBLE);
+    }
+
+    private void hideLoding(){
+        tvPostResult.setVisibility(View.VISIBLE);
+        pbLoadingData.setVisibility(View.INVISIBLE);
+    }
+
 }
